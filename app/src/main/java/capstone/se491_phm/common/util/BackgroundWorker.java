9e5742f1b2eeebc575112674ab7c8121a0fed67f
Tahani Alshehri <tahanimadah@gmail.com>
2016-11-10 02:41:36 -0600

    get registration token by primary key is completed

--- app/src/main/java/capstone/se491_phm/common/util/BackgroundWorker.java
+++ app/src/main/java/capstone/se491_phm/common/util/BackgroundWorker.java
@@ -57,6 +57,8 @@ public class BackgroundWorker extends AsyncTask<String,Void,String> {
         String save_URL= "http://phm.oregonresearchmethod.org/gcm.php";
         String getID_URL= "http://phm.oregonresearchmethod.org/getID.php";
         String getToken_URL= "http://phm.oregonresearchmethod.org/getToken.php";
+        String getTokenByID_URL= "http://phm.oregonresearchmethod.org/getTokenByID.php";
+
 
         if (type.equals("login"))
         {
@@ -227,6 +229,43 @@ public class BackgroundWorker extends AsyncTask<String,Void,String> {
                 OutputStream outputStream=httpURLConnection.getOutputStream();
                 BufferedWriter bufferedWriter= new BufferedWriter(new OutputStreamWriter(outputStream,"UTF-8"));
                 String post_data= URLEncoder.encode("deviceid","UTF-8")+"="+URLEncoder.encode(deviceid,"UTF-8");
+                bufferedWriter.write(post_data);
+                bufferedWriter.flush();
+                bufferedWriter.close();
+                outputStream.close();
+                InputStream inputStream=httpURLConnection.getInputStream();
+                BufferedReader bufferedReader=new BufferedReader(new InputStreamReader(inputStream,"iso-8859-1"));
+                String result="";
+                String line="";
+
+                while((line=bufferedReader.readLine())!=null)
+                {
+                    result+=line;
+                }
+                bufferedReader.close();
+                inputStream.close();
+                httpURLConnection.disconnect();
+                return result;
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }else if (type.equals("getTokenByID"))
+        {
+            try{
+                String id=params[1];
+
+                URL url= new URL(getTokenByID_URL);
+                HttpURLConnection httpURLConnection=(HttpURLConnection) url.openConnection();
+                httpURLConnection.setRequestMethod("POST");
+                httpURLConnection.setDoOutput(true);
+                httpURLConnection.setDoInput(true);
+                OutputStream outputStream=httpURLConnection.getOutputStream();
+                BufferedWriter bufferedWriter= new BufferedWriter(new OutputStreamWriter(outputStream,"UTF-8"));
+                String post_data= URLEncoder.encode("id","UTF-8")+"="+URLEncoder.encode(id,"UTF-8");
                 bufferedWriter.write(post_data);
                 bufferedWriter.flush();
                 bufferedWriter.close();
